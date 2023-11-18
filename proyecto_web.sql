8a930568939640ea083079d734dd5fd107594a8c
Diego <enviaradiegobernal@gmail.com>
2023-11-17 23:22:52 -0500

    SQL Completo

--- proyecto_web.sql
+++ proyecto_web.sql
@@ -1,11 +1,5 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+CREATE DATABASE proyecto_web;
+use proyecto_web;
 
 CREATE TABLE tbl_empleados (
   id int(11) NOT NULL,
@@ -16,20 +10,19 @@ CREATE TABLE tbl_empleados (
   cv varchar(255) NOT NULL,
   idpuesto int(11) NOT NULL,
   fechaingreso date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 CREATE TABLE tbl_puestos (
   id int(11) NOT NULL,
   nombredelpuesto varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 CREATE TABLE tbl_usuarios (
   id int(11) NOT NULL,
   usuario varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
   correo varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+);
 
 ALTER TABLE tbl_empleados
   ADD PRIMARY KEY (id),
@@ -41,7 +34,6 @@ ALTER TABLE tbl_puestos
 ALTER TABLE tbl_usuarios
   ADD PRIMARY KEY (id);
 
-
 ALTER TABLE tbl_empleados
   MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 
@@ -51,10 +43,5 @@ ALTER TABLE tbl_puestos
 ALTER TABLE tbl_usuarios
   MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 
-
 ALTER TABLE tbl_empleados
   ADD CONSTRAINT tbl_empleados_ibfk_1 FOREIGN KEY (idpuesto) REFERENCES tbl_puestos (id);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
